8775547a90f3b96ee0938d66c551f9bc5253d799
Kacper <kapi98981@gmail.com>
2025-06-12 18:50:49 +0200

    Rubber Adjust

--- src/Components/useDrawing.ts
+++ src/Components/useDrawing.ts
@@ -16,57 +16,65 @@ export function useDrawing(
 		if (!canvasRef.current || !ctx) return;
 		const canvas = canvasRef.current;
 
-		const startDrawing = (event: MouseEvent) => {
-			if (pickedTool !== "pencil") return;
-			const offsetX = event.offsetX;
-			const offsetY = event.offsetY;
-			ctx?.beginPath();
-			ctx.lineWidth = thickness;
-			ctx.strokeStyle = color;
-			ctx?.moveTo(offsetX, offsetY);
-			isDrawing.current = true;
+		const mouseDownHandler = (event: MouseEvent) => {
+			if (pickedTool === "pencil") {
+				const offsetX = event.offsetX;
+				const offsetY = event.offsetY;
+				ctx?.beginPath();
+				ctx.lineWidth = thickness;
+				ctx.strokeStyle = color;
+				ctx?.moveTo(offsetX, offsetY);
+				isDrawing.current = true;
+			}
+			else if (pickedTool === "rubber") {
+				ctx.clearRect(event.offsetX, event.offsetY, 35, 35);
+				isDrawing.current = true;
+			}
 		};
 
-		const draw = (event: MouseEvent) => {
-			if (!isDrawing.current || pickedTool !== "pencil") return;
-			const offsetX = event.offsetX;
-			const offsetY = event.offsetY;
-			ctx?.lineTo(offsetX, offsetY);
-			ctx?.stroke();
+		const mouseMoveHandler = (event: MouseEvent) => {
+			if (isDrawing.current && pickedTool === "pencil") {
+				const offsetX = event.offsetX;
+				const offsetY = event.offsetY;
+				ctx?.lineTo(offsetX, offsetY);
+				ctx?.stroke();
 
-			// if (connection) {
-			// 	// Stwórz obiekt z danymi do wysłania
-			// 	const lineData = {
-			// 		x: offsetX,
-			// 		y: offsetY,
-			// 		isDrawing: isDrawing,
-			// 	};
+				// if (connection) {
+				// 	// Stwórz obiekt z danymi do wysłania
+				// 	const lineData = {
+				// 		x: offsetX,
+				// 		y: offsetY,
+				// 		isDrawing: isDrawing,
+				// 	};
 
-			// 	// Wywołaj metodę na serwerze
-			// 	try {
-			// 		connection.invoke("DrawLine", lineData);
-			// 	} catch (e) {
-			// 		console.error("Błąd wysyłania danych: ", e);
-			// 	}
-			// }
+				// 	// Wywołaj metodę na serwerze
+				// 	try {
+				// 		connection.invoke("DrawLine", lineData);
+				// 	} catch (e) {
+				// 		console.error("Błąd wysyłania danych: ", e);
+				// 	}
+				// }
+			}
+			else if (pickedTool === "rubber" && isDrawing.current) ctx.clearRect(event.offsetX, event.offsetY, 35, 35);
 		};
 
-		const stopDrawing = () => {
-			if (!isDrawing.current || pickedTool !== "pencil") return;
-			isDrawing.current = false;
-			ctx?.closePath();
+		const mouseUpHandler = (event: MouseEvent) => {
+			if (pickedTool === "rubber" && isDrawing.current) isDrawing.current = false;
+			else if (isDrawing.current && pickedTool == "pencil") {
+				isDrawing.current = false;
+				ctx?.closePath();
+			}
 		};
-
-		canvas?.addEventListener("mousedown", startDrawing);
-		canvas?.addEventListener("mousemove", draw);
-		canvas?.addEventListener("mouseup", stopDrawing);
-		canvas?.addEventListener("mouseleave", stopDrawing);
+		canvas?.addEventListener("mousedown", mouseDownHandler);
+		canvas?.addEventListener("mousemove", mouseMoveHandler);
+		canvas?.addEventListener("mouseup", mouseUpHandler);
+		canvas?.addEventListener("mouseleave", mouseUpHandler);
 
 		return () => {
-			canvas?.removeEventListener("mousedown", startDrawing);
-			canvas?.removeEventListener("mousemove", draw);
-			canvas?.removeEventListener("mouseup", stopDrawing);
-			canvas?.removeEventListener("mouseleave", stopDrawing);
+			canvas?.removeEventListener("mousedown", mouseDownHandler);
+			canvas?.removeEventListener("mousemove", mouseMoveHandler);
+			canvas?.removeEventListener("mouseup", mouseUpHandler);
+			canvas?.removeEventListener("mouseleave", mouseUpHandler);
 		}
 	}, [ctx, pickedTool, color]);
 
